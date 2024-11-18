4152761a79e132e143cb2746769a4ffc0a081716
Sanger Steel <sangersteel@gmail.com>
2024-11-17 22:51:53 -0500

    chore: Reformat `main.c`

--- src/main.c
+++ src/main.c
@@ -10,17 +10,18 @@
 //  Each worker should be in an infinite loop waiting for "tasks" to do from the channels
 
 int main(void) {
-    FILE* file = fopen("../data/test.txt", "r");
+    FILE *file = fopen("../data/test.txt", "r");
     if (file == NULL) {
         perror("Error opening file");
         return 1;
     }
 
-    context* ctx = new_context(file);
+    context *ctx = new_context(file);
 
     CHECK_ERR(write_messages_to_channel(ctx), "Writer thread creation failed.");
     CHECK_ERR(read_data_from_channel(ctx), "Reader thread creation failed.");
-    CHECK_ERR(wait_on_writing_thread(ctx), "Wait for writer threads termination failed.");
+    CHECK_ERR(wait_on_writing_thread(ctx),
+              "Wait for writer threads termination failed.");
     WAIT_THREAD(ctx, data_reading_thread_pool);
 
     destroy_context(ctx);
